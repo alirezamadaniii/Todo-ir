9ccc9353fd7fb4a1ad94c7ba082de8e64ec75a6c
alirezamadaniii <alirezamadani33@gmail.com>
2024-01-29 10:19:48 +0330

    add bottom navigation

--- app/src/main/java/com/example/todoir/MainActivity.kt
+++ app/src/main/java/com/example/todoir/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.todoir
 
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.todoir.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
+
     }
 }
